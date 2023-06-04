aebba0ccc047193cef6c57a4a5423537eeb332b6
qorsan73 <99475446+qorsan73@users.noreply.github.com>
2023-06-04 09:54:57 +0300

    Add files via upload

---
+++ key.py
@@ -0,0 +1,41 @@
+import os
+try :
+    import pyfiglet
+    import requests
+except:
+    os.system('pip3 install pyfiglet')
+    os.system('pip3 install requests')    
+Z =  '\033[1;31m' 
+F = '\033[2;32m' 
+B = '\033[2;36m'
+X = '\033[1;33m' 
+C = '\033[2;35m'
+
+logo = pyfiglet.figlet_format('             Qorsan ')
+print(Z+logo)
+print('my namber', +967737572399)
+print("\n\033[1;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n")
+print("Select operation:")
+print("1. paylod image")
+print("2. paylod APK")
+print("3. my youtube ")
+print("4. my telegram")
+degree = int (input("Enter operation number (1): "))
+
+qorsan = ('bash pig.sh' )
+
+taiz = ("bash APK.sh")
+
+mostafa = ()
+aldobaei = ()
+
+if degree ==  1:
+    print(qorsan)
+elif degree ==   2:
+    print(taiz)
+elif degree == 3:
+    print(mostafa)
+elif degree == 4:
+    print(aldobaei)
+else:
+    print("Invalid input!")
