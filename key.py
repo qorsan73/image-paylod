c55f331358dfb5a01326a99138874ba7525430bc
qorsan taiz <99475446+qorsan73@users.noreply.github.com>
2023-09-24 06:28:21 +0300

    Update key.py

--- key.py
+++ key.py
@@ -2,7 +2,6 @@
 #"https://github.com/qorsan73"
 import os
 try :
-    import pyfiglet
     import requests
 except:
     os.system("clear")
@@ -12,8 +11,7 @@ B = '\033[2;36m'
 X = '\033[1;33m' 
 C = '\033[2;35m'
 
-logo = pyfiglet.figlet_format('             Qorsan ')
-print(Z+logo)
+print(QORSAN TAIZ)
 print('my namber:', +967737572399)
 print('github:' ,"https://github.com/qorsan73")
 print('by:' ,"qorsan taiz")
